449ee8a3bbe578da95fa5fa8b790a3caa42ef921
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-19 13:35:04 +0300

    added searchOrder

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Pizza Co.</Link>
+
+      <SearchOrder />
 
       <p>Lilan</p>
     </header>
