03edb7d94549bcf8dc378c07bafbafcf6853bd24
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-19 13:45:44 +0300

    added searchOrder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 
@@ -20,7 +20,12 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
       { path: "/order/new", element: <CreateOrder /> },
     ],
   },
