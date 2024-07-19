0e9826054c13031187db759a63dff52d205faef2
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-19 12:25:55 +0300

    added AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { pathe: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/new", element: <CreateOrder /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizza Co.</Link>
+
+      <p>Lilan</p>
+    </header>
+  );
+}
+
+export default Header;
