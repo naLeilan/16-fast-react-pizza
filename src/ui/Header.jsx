183ef4e2abe48c49938f38f11b76d38b4d608750
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-21 10:50:20 +0300

    styled text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Lilan</p>
+      <Username />
     </header>
   );
 }
