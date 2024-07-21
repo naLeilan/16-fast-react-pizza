cc076ab1f33abbed8c65dd9a37a6da71a6fb7b32
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-21 09:46:20 +0300

    set up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
