4f9c4cedd639d406fbe52631cc683f4d25333d8f
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-21 11:01:08 +0300

    styled boxmodel

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">lilan</div>;
+  return <div className="hidden text-sm font-semibold">lilan</div>;
 }
 
 export default Username;
